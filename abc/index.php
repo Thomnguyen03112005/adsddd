bba707cdabe5945cf053d7ada72d681203cfc92a
Thowm <83901352+Thomnguyen03112005@users.noreply.github.com>
2022-02-20 04:30:44 +0700

    Add files via upload

---
+++ abc/index.php
@@ -0,0 +1,18 @@
+<?php 
+    session_start();
+    include("connection.php");
+    include("fuctions.php");
+    $user_data = check_login($con);
+
+
+?>
+
+<!Doctype html>
+
+<html> 
+    <head>
+        <p>welcome</p>
+    </head>    
+
+
+</html>
